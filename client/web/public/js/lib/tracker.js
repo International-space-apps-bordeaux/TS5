9773b82124bd3f3d37abad21b1f392f7ff10a183
Thomas Pinaud <pinaudt@gmail.com>
2014-04-12 22:24:03 +0200

    Added orbit path on map. Added possibility to switch satellite. Position are updated each 2 minutes, so position is interpolated each second to update the map.

---
+++ client/web/public/js/lib/tracker.js
@@ -0,0 +1,140 @@
+var map;
+var markers = [];
+var paths   = [];
+
+var app = {
+    satId: "iss",
+    way: 1,
+    timer: null,
+    thick: 0,
+
+    init: function() {
+        this.bindList();
+        this.liveUpdatePosition();
+    },
+    
+    bindList: function() {
+        var _this = this;
+        $('#sat-list li').each(function(k, el){
+            $(el).click(function() {
+                _this.satId = $(this).data('idsat');
+                _this.way   = $(this).data('way');
+                _this.liveUpdatePosition();
+                $('#current-sat').html($(this).html());
+            })
+        });
+    },
+
+    liveUpdatePosition: function() {
+        //Start requesting satellite position each 2 minutes
+        var _this = this;
+        this.cleanOrbits();
+        setInterval(function() {
+            _this.getPositions();
+        }, 120000);
+        _this.getPositions();
+    },
+
+    updatePosition: function() {
+
+        this.thick++;
+        var startDate = new Date(this.t1.date);
+        var endDate = new Date(this.t2.date);
+        var seconds = startDate.getSeconds();
+
+        if(startDate.getMinutes() == endDate.getMinutes()) {
+            seconds += 60;
+        }
+
+        var deltaLat = ((this.t2.latitude - this.t1.latitude) / 120) * this.thick;
+        var deltaLng = (this.t2.longitude - this.t1.longitude);
+        //If satellite pass over Greenwich, I add a 360 loop on the longitude to avoid a return to 0
+        if(this.way == 1) {
+            deltaLng = deltaLng < 0 ? deltaLng + 360 : deltaLng;
+        }
+        deltaLng = (deltaLng / 120) * this.thick;
+
+        var lat = deltaLat + this.t1.latitude;
+        var lng = deltaLng + this.t1.longitude;
+
+        var latLng = new google.maps.LatLng(lat, lng);
+
+        this.cleanMarkers();
+
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: this.satId
+        });
+
+        markers.push(marker);
+        map.panTo(latLng)
+    },
+
+    cleanMarkers: function() {
+        for(i in markers) {
+            markers[i].setMap(null);
+        }
+    },
+
+    cleanOrbits: function() {
+        for(i in paths) {
+            paths[i].setMap(null);
+        }
+    },
+
+    getPositions: function() {
+        var _this = this;
+
+        var url = "http://10.33.0.33:8080/SatelliteServiceClient/api/satellites/trajectory/"+this.satId;
+
+        $.getJSON(url, function(data) {
+
+            console.info('Data from server');
+            console.log(data);
+
+            if(_this.timer) {
+                clearInterval(_this.timer);
+            }
+            _this.thick = 0;
+
+            var t1 = data[0];
+            var t2 = data[1];
+
+            _this.printOrbit(t1, t2);
+
+            _this.t1 = t1;
+            _this.t2 = t2;
+
+            _this.timer = setInterval(function() {
+                _this.updatePosition();
+            }, 1000);
+        });
+    },
+
+    printOrbit: function(pos1, pos2) {
+        var latLng2 = new google.maps.LatLng(pos2.latitude, pos2.longitude);
+
+
+        var OrbitCoordinates = [
+            new google.maps.LatLng(pos1.latitude, pos1.longitude),
+            new google.maps.LatLng(pos2.latitude, pos2.longitude)
+        ];
+        var orbitPath = new google.maps.Polyline({
+            path: OrbitCoordinates,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+        paths.push(orbitPath)
+
+        orbitPath.setMap(map);
+    }
+
+}
+
+$(function() {
+    app.init();
+})
