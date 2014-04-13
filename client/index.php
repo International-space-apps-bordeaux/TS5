e08a901a86420c1e2df8e6c3b6834de357d083e6
root <root@mengellatte-thomas.(none)>
2014-04-13 11:28:26 +0200

    iss aim fast

--- client/index.php
+++ client/index.php
@@ -11,6 +11,7 @@ $baseUrl = "";
 
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBYOgoKO_QD7cFxGwfqenxkch9SZlU59bU&sensor=false"></script>
         <script src="http://<?php echo $_SERVER['SERVER_NAME']."/".$baseUrl; ?>FollowSat/client/js/tracker.js"></script>
+	<link rel="icon" href="favicon.ico"/>
 </head>
 <body>
     
@@ -28,25 +29,28 @@ $baseUrl = "";
               <li class="selected" data-idsat="iss" data-way="1"><p>ISS</p></li>
               <li data-idsat="aim" data-way="0"><p>AIM</p></li>
               <li data-idsat="fast" data-way="0"><p>Fast</p></li>
-              <li data-idsat="iss" data-way="1"><p>ISS</p></li>
-              <li data-idsat="aim" data-way="0"><p>AIM</p></li>
-            <li data-idsat="fast" data-way="0"><p>Fast</p></li>
             </ul>
 	  </div>
 	</div>
       </div>
       <div id="document">
+	<div id="iss" class="description">
 	<p>
-	  The International Space Station (ISS) is a space station, or a habitable artificial satellite in low Earth orbit. It is a modular structure whose first component was launched in 1998.[7] Now the largest artificial body in orbit, it can often be seen at the appropriate time with the naked eye from Earth.[8] The ISS consists of pressurised modules, external trusses, solar arrays and other components. ISS components have been launched by American Space Shuttles as well as Russian Proton and Soyuz rockets.[9] In 1984 the ESA was invited to participate in Space Station Freedom.[10] In 1993, after the USSR ended, the United States and Russia merged Mir-2 and Freedom together.[9]
-
-The ISS serves as a microgravity and space environment research laboratory in which crew members conduct experiments in biology, human biology, physics, astronomy, meteorology and other fields.[11][12][13] The station is suited for the testing of spacecraft systems and equipment required for missions to the Moon and Mars.[14]
-
-Since the arrival of Expedition 1 on 2 November 2000, the station has been continuously occupied for 13 years and 162 days, the longest continuous human presence in space. (In 2010, the station surpassed the previous record of almost 10 years (or 3,634 days) held by Mir.) The station is serviced by a variety of visiting spacecraft: Soyuz, Progress, the Automated Transfer Vehicle, the H-II Transfer Vehicle,[15] Dragon, and Cygnus. It has been visited by astronauts and cosmonauts from 15 different nations.[16]
-
-After the U.S. Space Shuttle program ended in 2011, Soyuz rockets became the only provider of transport for astronauts at the International Space Station, while Dragon became the only provider of cargo-return-to-Earth services.
-
-The ISS programme is a joint project among five participating space agencies: NASA, Roskosmos, JAXA, ESA, and CSA.[15][17] The ownership and use of the space station is established by intergovernmental treaties and agreements.[18] The station is divided into two sections, the Russian Orbital Segment (ROS) and the United States Orbital Segment (USOS), which is shared by many nations. The ISS maintains an orbit with an altitude of between 330 km (205 mi) and 435 km (270 mi) by means of reboost manoeuvres using the engines of the Zvezda module or visiting spacecraft. It completes 15.50 orbits per day.[19] The ISS is funded until 2024, and may operate until 2028.[20][21][22] The Russian Federal Space Agency, Roskosmos (RKA) has proposed using the ISS to commission modules for a new space station, called OPSEK, before the remainder of the ISS is deorbited. ISS is the ninth space station to be inhabited by crews, following the Soviet and later Russian Salyut, Almaz, and Mir stations, and Skylab from the US.
+The International Space Station (ISS) is a space station, a habitable artificial satellite in low Earth orbit. The ISS serves as a microgravity and space environment research laboratory in which crew members conduct experiments in biology, human biology, physics, astronomy, meteorology and other fields. The station is suited for the testing of spacecraft systems and equipment required for missions to the Moon and Mars.
 	</p>
+	</div>
+	<div id="aim" class="description">
+	<p>
+The Aeronomy of Ice in the Mesosphere (AIM) satellite mission is exploring Polar Mesospheric Clouds (PMCs), also called noctilucent clouds, to find out why they form and why they are changing. 
+
+The primary goal of the AIM mission is to help scientists understand whether the clouds' ephemeral nature, and their variation over time, is related to Earth's changing climate - and to investigate why they form in the first place. By measuring the thermal, chemical and other properties of the environment in which the mysterious clouds form, the AIM mission will provide researchers with a foundation for the study of long-term variations in the mesosphere and its relationship to global climate change. In addition to measuring environmental conditions, the AIM mission will collect data on cloud abundance, how the clouds are distributed, and the size of particles within them.
+	</p>
+	</div>
+	<div id="fast" class="description">
+	<p>
+The FAST (Fast Auroral Snapshot Explorer) satellite is a highly sophisticated scientific satellite designed to carry out in situ measurements of acceleration physics and related plasma processes associated with the Earth's aurora.  Initiated and conceptualized by scientists at the University of California at Berkeley, is designed to carry out small, highly focused, scientific investigations. The spacecraft design was tailored to take high-resolution data samples (or "snapshots") only while it crosses the auroral zones, which are latitudinally narrow sectors that encircle the polar regions of the Earth.  FAST's primary objective is to study the microphysics of space plasma and the accelerated particles that cause the aurora.</br>FAST was launched on August 21, 1996 into a high inclination (83o) elliptical orbit with apogee and perigee altitudes of 4175 km and 350 km, respectively.
+	</p>
+	</div>
       </div>
 
     </div>
